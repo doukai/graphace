3c8cef401143c6b302805fa29be81f0dc841f139
Kai <doukai.java@gmail.com>
2022-11-03 00:07:18 +0800

    update

---
+++ packages/config/dist/index.cjs
@@ -0,0 +1,9 @@
+const postcssConfig = require('./postcss.config.cjs');
+const tailwindConfig = require('./tailwind.config.cjs');
+const eslintConfig = require('./.eslintrc.cjs');
+
+module.exports = {
+    postcssConfig,
+    tailwindConfig,
+    eslintConfig
+};
