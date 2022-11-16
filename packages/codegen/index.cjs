a20b14ca0334568d9ea1139b9b37bb9fb0d72574
Kai <doukai.java@gmail.com>
2022-11-17 01:17:35 +0800

    update

--- packages/codegen/index.cjs
+++ packages/codegen/index.cjs
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const { isObjectType, isScalarType, isNonNullType, assertNamedType } = require("graphql/type/definition");
 const helpers = require("@graphql-codegen/plugin-helpers");
 const path = require("path");
 const plugin = async (schema, _documents, pluginConfig) => {
@@ -9,27 +8,30 @@ const plugin = async (schema, _documents, pluginConfig) => {
         .map(key => queryFields[key])
         .map(field =>
             `query ${field.name.charAt(0).toUpperCase() + field.name.slice(1)}${field.args && field.args.length > 0 ? '(' + field.args.map(arg => '$' + arg.name + ': ' + arg.type).join(', ') + ')' : ''} {
-    ${field.name}${field.args && field.args.length > 0 ? '(' + field.args.map(arg => arg.name + ': $' + arg.name).join(', ') + ')' : ''} ${getSelections(schema, field)}
+    ${field.name}${field.args && field.args.length > 0 ? '(' + field.args.map(arg => arg.name + ': $' + arg.name).join(', ') + ')' : ''} ${getSelections(field)}
 }
         `);
 
-    return queries.join('\r\n');
+    return {
+        prepend: ['1', '2', '3'],
+        content: queries.join('\r\n')
+    };
 };
 
-const getSelections = (schema, field) => {
-    // const fieldType = schema.getType(field.type);
-    // isObjectType(field.type);
-    // if (isObjectType(fieldType)) {
-    //     assertObjectType(fieldType);
-    // }
-    // isNamedType(fieldType);
-    //     if (isObjectType(fieldType)) {
-    //         return `{
-    // ${fieldType.getFields().filter(field => isScalarType(getType(field.type))).map(field => JSON.stringify(field)).join('\r\n')}
-    // }
-    //         `;
-    //     }
-    return schema.getType(getFieldType(field.type)).getFields ? JSON.stringify(schema.getType(getFieldType(field.type)).getFields()) : '';
+const aggregateSuffix = ["Count", "Sum", "Avg", "Max", "Min", "Aggregate"];
+
+const getSelections = (field) => {
+    const fieldType = getFieldType(field.type);
+    if (fieldType._fields) {
+        return `{
+        ${Object.values(fieldType._fields)
+                .filter(field => !getFieldType(field.type)._fields)
+                .filter(field => !aggregateSuffix.some(suffix => field.name.slice(-suffix.length) === suffix))
+                .map(field => field.name)
+                .join('\r\n\t\t')}
+    }`;
+    }
+    return '';
 }
 
 const getFieldType = (type) => {
