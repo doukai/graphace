26ce657973326de777ed225433758095fadeaec9
Kai <doukai.java@gmail.com>
2023-01-15 00:36:51 +0800

    update

--- apps/web/src/lib/components/types/user/Table.svelte
+++ apps/web/src/lib/components/types/user/Table.svelte
@@ -1,41 +1,49 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import { __schema } from '~/gql/generated/introspection.json';
+	import { createEventDispatcher } from 'svelte';
 	import { StringTh, StringTd } from '@graphace/ui-graphql/components/table';
-	import { SectionHead, SectionLoading } from '@graphace/ui/components/section';
+	import { SectionHead } from '@graphace/ui/components/section';
 	import { Table, TableLoading } from '@graphace/ui/components/table';
 	import SearchInput from '@graphace/ui/components/search/SearchInput.svelte';
-	import { notifications } from '@graphace/ui/components/Notifications.svelte';
 	import { messageBoxs } from '@graphace/ui/components/MessageBoxs.svelte';
+	import { notifications } from '@graphace/ui/components/Notifications.svelte';
 	import { Icon } from '@steeze-ui/svelte-icon';
 	import { Plus, PencilAlt, Trash } from '@steeze-ui/heroicons';
 	import LL from '~/i18n/i18n-svelte';
 	import { locale } from '~/i18n/i18n-svelte';
+	import { validateUpdate } from '@graphace/graphql/schema/JsonSchema';
 	import { Pagination } from '@graphace/ui/components/connection';
 	import {
 		Conditional,
 		Operator,
 		User,
 		UserOrderBy,
-		QueryTypeUserListArgs
+		QueryTypeUserListArgs,
+		MutationTypeUserArgs
 	} from '~/gql/generated/schema';
-	import {
-		QueryUserConnection$input,
-		GQL_UpdateUser,
-		UpdateUser$input,
-		QueryUserConnection$result
-	} from '$houdini';
-	import { validateUpdate } from '@graphace/graphql/schema/JsonSchema';
 
-	export let isFetching: boolean;
-	export let nodes: (User | null | undefined)[];
+	export let nodes: (User | null | undefined)[] | null | undefined;
 	export let totalCount: number = 0;
+	export let isFetching: boolean;
+
+	const dispatch = createEventDispatcher<{
+		fetch: {
+			args: QueryTypeUserListArgs;
+			then: (data: (User | null | undefined)[] | null | undefined) => void;
+			catch: (error: Error) => void;
+		};
+		mutation: {
+			args: MutationTypeUserArgs;
+			then: (data: User | null | undefined) => void;
+			catch: (error: Error) => void;
+		};
+	}>();
 
 	let errors: Record<string, Record<string, Error>> = {};
 
 	let showDeleteButton = false;
 	let queryValue: string | undefined;
-	let variables: QueryTypeUserListArgs = {};
+	let args: QueryTypeUserListArgs = {};
 	let orderBy: UserOrderBy = {};
 	let after: string | undefined;
 	let before: string | undefined;
@@ -58,84 +66,96 @@
 
 	const query = () => {
 		if (queryValue) {
-			variables = {};
-			variables.cond = Conditional.OR;
-			variables.login = { opr: Operator.LK, val: `%${queryValue}%` };
-			variables.name = { opr: Operator.LK, val: `%${queryValue}%` };
-			variables.phones = { opr: Operator.LK, val: `%${queryValue}%` };
+			args = {};
+			args.cond = Conditional.OR;
+			args.login = { opr: Operator.LK, val: `%${queryValue}%` };
+			args.name = { opr: Operator.LK, val: `%${queryValue}%` };
+			args.phones = { opr: Operator.LK, val: `%${queryValue}%` };
 		} else {
 			if (Object.keys(orderBy).length > 0) {
-				variables.orderBy = orderBy;
+				args.orderBy = orderBy;
 			} else {
-				variables.orderBy = undefined;
+				args.orderBy = undefined;
 			}
 		}
 
 		if (after) {
-			variables.after = after;
-			variables.first = pageSize;
+			args.after = after;
+			args.first = pageSize;
 		} else if (before) {
-			variables.before = before;
-			variables.last = pageSize;
+			args.before = before;
+			args.last = pageSize;
 		} else if (offset) {
-			variables.offset = offset;
-			variables.first = pageSize;
+			args.offset = offset;
+			args.first = pageSize;
 		} else {
-			variables.first = pageSize;
+			args.first = pageSize;
 		}
 
-		QueryUserConnection.fetch({ variables });
+		dispatch('fetch', {
+			args,
+			then: (data) => {},
+			catch: (error) => {
+				notifications.error($LL.message.requestFailed());
+			}
+		});
 	};
 
-	async function updateField(node: UpdateUser$input | null | undefined) {
-		if (node && node.id) {
-			errors[node.id] = {};
-			validateUpdate('User', node, $locale)
+	async function updateField(args: MutationTypeUserArgs | null | undefined) {
+		if (args && args.id) {
+			errors[args.id] = {};
+			validateUpdate('User', args, $locale)
 				.then((data) => {
-					if (node) {
-						GQL_UpdateUser.mutate(node)
-							.then(() => {
+					if (args) {
+						dispatch('mutation', {
+							args,
+							then: (data) => {
 								notifications.success($LL.message.saveSuccess());
-							})
-							.catch((error) => {
+							},
+							catch: (error) => {
 								console.error(error);
 								notifications.error($LL.message.saveFailed());
-							});
+							}
+						});
 					}
 				})
 				.catch((validErrors) => {
-					if (node.id) {
-						errors[node.id] = validErrors;
+					if (args.id) {
+						errors[args.id] = validErrors;
 					}
 				});
 		}
 	}
 
 	async function removeRow(id: string) {
-		GQL_UpdateUser.mutate({ id: id, isDeprecated: true })
-			.then(() => {
+		dispatch('mutation', {
+			args: { id: id, isDeprecated: true },
+			then: (data) => {
 				notifications.success($LL.message.removeSuccess());
 				query();
-			})
-			.catch((error) => {
+			},
+			catch: (error) => {
 				console.error(error);
-				notifications.error($LL.message.requestFailed());
-			});
+				notifications.error($LL.message.removeFailed());
+			}
+		});
 	}
 
 	const removeRows = () => {
-		GQL_UpdateUser.mutate({
-			isDeprecated: true,
-			where: { id: { opr: Operator.IN, in: selectedIdList } }
-		})
-			.then(() => {
+		dispatch('mutation', {
+			args: {
+				where: { id: { opr: Operator.IN, in: selectedIdList } },
+				isDeprecated: true
+			},
+			then: (data) => {
 				notifications.success($LL.message.removeSuccess());
 				query();
-			})
-			.catch((error) => {
+			},
+			catch: (error) => {
 				console.error(error);
 				notifications.error($LL.message.removeFailed());
-			});
+			}
+		});
 	};
 </script>
 
@@ -194,30 +214,32 @@
 						class="checkbox"
 						bind:checked={selectAll}
 						on:change={() => {
-							nodes.forEach((node) => {
-								if (node?.id) {
-									selectedRows[node.id] = selectAll;
-								}
-							});
+							if (nodes && nodes.length > 0) {
+								nodes.forEach((node) => {
+									if (node?.id) {
+										selectedRows[node.id] = selectAll;
+									}
+								});
+							}
 						}}
 					/>
 				</label>
 			</th>
 			<StringTh
 				name="name"
-				bind:expression={variables.name}
+				bind:expression={args.name}
 				bind:sort={orderBy.name}
 				on:filter={query}
 			/>
 			<StringTh
 				name="login"
-				bind:expression={variables.login}
+				bind:expression={args.login}
 				bind:sort={orderBy.login}
 				on:filter={query}
 			/>
 			<StringTh
 				name="password"
-				bind:expression={variables.password}
+				bind:expression={args.password}
 				bind:sort={orderBy.password}
 				on:filter={query}
 			/>
@@ -228,71 +250,73 @@
 		<TableLoading />
 	{:else}
 		<tbody>
-			{#each nodes as node}
-				{#if node && node.id}
-					<tr class="hover">
-						<th class="z-10">
-							<label>
-								<input type="checkbox" class="checkbox" bind:checked={selectedRows[node.id]} />
-							</label>
-						</th>
-						<StringTd
-							name="name"
-							bind:value={node.name}
-							on:save={() => updateField({ id: node?.id, name: node?.name })}
-							error={errors[node.id]?.name}
-						/>
-						<StringTd
-							name="login"
-							bind:value={node.login}
-							on:save={() => updateField({ id: node?.id, login: node?.login })}
-							error={errors[node.id]?.login}
-						/>
-						<StringTd
-							name="password"
-							bind:value={node.password}
-							on:save={() => updateField({ id: node?.id, password: node?.password })}
-							error={errors[node.id]?.password}
-						/>
-						<td>
-							<div class="tooltip" data-tip={$LL.components.graphql.table.editBtn()}>
-								<button
-									class="btn btn-square btn-ghost btn-xs"
-									on:click={(e) => {
-										e.preventDefault();
-										if (node) {
-											goto(`./user/${node.id}`);
-										}
-									}}
-								>
-									<Icon src={PencilAlt} solid />
-								</button>
-							</div>
-							<div class="tooltip" data-tip={$LL.components.graphql.table.removeBtn()}>
-								<button
-									class="btn btn-square btn-ghost btn-xs"
-									on:click={(e) => {
-										e.preventDefault();
-										messageBoxs.open({
-											title: $LL.components.graphql.table.removeModalTitle(),
-											buttonName: $LL.components.graphql.table.removeBtn(),
-											buttonType: 'error',
-											confirm: () => {
-												if (node?.id) {
-													removeRow(node.id);
-												}
-												return true;
+			{#if nodes && nodes.length > 0}
+				{#each nodes as node}
+					{#if node && node.id}
+						<tr class="hover">
+							<th class="z-10">
+								<label>
+									<input type="checkbox" class="checkbox" bind:checked={selectedRows[node.id]} />
+								</label>
+							</th>
+							<StringTd
+								name="name"
+								bind:value={node.name}
+								on:save={() => updateField({ id: node?.id, name: node?.name })}
+								error={errors[node.id]?.name}
+							/>
+							<StringTd
+								name="login"
+								bind:value={node.login}
+								on:save={() => updateField({ id: node?.id, login: node?.login })}
+								error={errors[node.id]?.login}
+							/>
+							<StringTd
+								name="password"
+								bind:value={node.password}
+								on:save={() => updateField({ id: node?.id, password: node?.password })}
+								error={errors[node.id]?.password}
+							/>
+							<td>
+								<div class="tooltip" data-tip={$LL.components.graphql.table.editBtn()}>
+									<button
+										class="btn btn-square btn-ghost btn-xs"
+										on:click={(e) => {
+											e.preventDefault();
+											if (node) {
+												goto(`./user/${node.id}`);
 											}
-										});
-									}}
-								>
-									<Icon src={Trash} solid />
-								</button>
-							</div>
-						</td>
-					</tr>
-				{/if}
-			{/each}
+										}}
+									>
+										<Icon src={PencilAlt} solid />
+									</button>
+								</div>
+								<div class="tooltip" data-tip={$LL.components.graphql.table.removeBtn()}>
+									<button
+										class="btn btn-square btn-ghost btn-xs"
+										on:click={(e) => {
+											e.preventDefault();
+											messageBoxs.open({
+												title: $LL.components.graphql.table.removeModalTitle(),
+												buttonName: $LL.components.graphql.table.removeBtn(),
+												buttonType: 'error',
+												confirm: () => {
+													if (node?.id) {
+														removeRow(node.id);
+													}
+													return true;
+												}
+											});
+										}}
+									>
+										<Icon src={Trash} solid />
+									</button>
+								</div>
+							</td>
+						</tr>
+					{/if}
+				{/each}
+			{/if}
 		</tbody>
 	{/if}
 </Table>
--- apps/web/src/routes/[lang]/types/user/+page.svelte
+++ apps/web/src/routes/[lang]/types/user/+page.svelte
@@ -1,310 +1,49 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	import { __schema } from '~/gql/generated/introspection.json';
-	import { StringTh, StringTd } from '@graphace/ui-graphql/components/table';
-	import { SectionHead, SectionLoading } from '@graphace/ui/components/section';
-	import { Table, TableLoading } from '@graphace/ui/components/table';
-	import SearchInput from '@graphace/ui/components/search/SearchInput.svelte';
-	import { notifications } from '@graphace/ui/components/Notifications.svelte';
-	import { messageBoxs } from '@graphace/ui/components/MessageBoxs.svelte';
-	import { Icon } from '@steeze-ui/svelte-icon';
-	import { Plus, PencilAlt, Trash } from '@steeze-ui/heroicons';
-	import LL from '~/i18n/i18n-svelte';
-	import { locale } from '~/i18n/i18n-svelte';
-	import { Pagination } from '@graphace/ui/components/connection';
-	import {
-		Conditional,
-		Operator,
-		QueryUserConnection$input,
-		QueryUserConnectionStore,
-		UserOrderBy,
-		GQL_UpdateUser,
-		UpdateUser$input
-	} from '$houdini';
-	import { validateUpdate } from '@graphace/graphql/schema/JsonSchema';
+	import Table from '~/lib/components/types/user/Table.svelte';
+	import type { User, QueryTypeUserListArgs, MutationTypeUserArgs } from '~/gql/generated/schema';
+	import { QueryUserConnectionStore, GQL_UpdateUser } from '$houdini';
 	import type { PageData } from './$houdini';
 
 	export let data: PageData;
 	$: QueryUserConnection = data.QueryUserConnection as QueryUserConnectionStore;
-	$: nodes = $QueryUserConnection.data?.userConnection?.edges?.map((edge) => edge?.node) as (
-		| UpdateUser$input
-		| null
-		| undefined
-	)[];
-	$: totalCount = $QueryUserConnection.data?.userConnection?.totalCount || 0;
-	let errors: Record<string, Record<string, Error>> = {};
 
-	let showDeleteButton = false;
-	let queryValue: string | undefined;
-	let variables: QueryUserConnection$input = {};
-	let orderBy: UserOrderBy = {};
-	let after: string | undefined;
-	let before: string | undefined;
-	let pageNumber: number = 1;
-	let pageSize: number = 10;
-	$: offset = (pageNumber - 1) * pageSize;
-
-	let selectAll: boolean;
-	let selectedRows: Record<string, boolean> = {};
-
-	$: selectedIdList = Object.keys(selectedRows)
-		.filter((id) => selectedRows[id])
-		.map((id) => id);
-
-	$: if (selectedIdList.length > 0) {
-		showDeleteButton = true;
-	} else {
-		showDeleteButton = false;
-	}
-
-	const query = () => {
-		if (queryValue) {
-			variables = {};
-			variables.cond = Conditional.OR;
-			variables.login = { opr: Operator.LK, val: `%${queryValue}%` };
-			variables.name = { opr: Operator.LK, val: `%${queryValue}%` };
-			variables.phones = { opr: Operator.LK, val: `%${queryValue}%` };
-		} else {
-			if (Object.keys(orderBy).length > 0) {
-				variables.orderBy = orderBy;
-			} else {
-				variables.orderBy = undefined;
-			}
-		}
-
-		if (after) {
-			variables.after = after;
-			variables.first = pageSize;
-		} else if (before) {
-			variables.before = before;
-			variables.last = pageSize;
-		} else if (offset) {
-			variables.offset = offset;
-			variables.first = pageSize;
-		} else {
-			variables.first = pageSize;
-		}
-
-		QueryUserConnection.fetch({ variables });
+	const fetch = (
+		event: CustomEvent<{
+			args: QueryTypeUserListArgs;
+			then: (data: (User | null | undefined)[] | null | undefined) => void;
+			catch: (error: Error) => void;
+		}>
+	) => {
+		QueryUserConnection.fetch({ variables: event.detail.args })
+			.then((result) => {
+				event.detail.then(result.data?.userConnection?.edges?.map((edge) => edge?.node));
+			})
+			.catch((error) => {
+				event.detail.catch(error);
+			});
 	};
 
-	async function updateField(node: UpdateUser$input | null | undefined) {
-		if (node && node.id) {
-			errors[node.id] = {};
-			validateUpdate('User', node, $locale)
-				.then((data) => {
-					if (node) {
-						GQL_UpdateUser.mutate(node)
-							.then(() => {
-								notifications.success($LL.message.saveSuccess());
-							})
-							.catch((error) => {
-								console.error(error);
-								notifications.error($LL.message.saveFailed());
-							});
-					}
-				})
-				.catch((validErrors) => {
-					if (node.id) {
-						errors[node.id] = validErrors;
-					}
-				});
-		}
-	}
-
-	async function removeRow(id: string) {
-		GQL_UpdateUser.mutate({ id: id, isDeprecated: true })
-			.then(() => {
-				notifications.success($LL.message.removeSuccess());
-				query();
+	const mutation = (
+		event: CustomEvent<{
+			args: MutationTypeUserArgs;
+			then: (data: User | null | undefined) => void;
+			catch: (error: Error) => void;
+		}>
+	) => {
+		GQL_UpdateUser.mutate(event.detail.args)
+			.then((result) => {
+				event.detail.then(result?.user);
 			})
 			.catch((error) => {
-				console.error(error);
-				notifications.error($LL.message.requestFailed());
-			});
-	}
-
-	const removeRows = () => {
-		GQL_UpdateUser.mutate({
-			isDeprecated: true,
-			where: { id: { opr: Operator.IN, in: selectedIdList } }
-		})
-			.then(() => {
-				notifications.success($LL.message.removeSuccess());
-				query();
-			})
-			.catch((error) => {
-				console.error(error);
-				notifications.error($LL.message.removeFailed());
+				event.detail.catch(error);
 			});
 	};
 </script>
 
-<SectionHead title="User">
-	<SearchInput bind:value={queryValue} on:search={query} />
-	{#if showDeleteButton}
-		<div class="tooltip tooltip-bottom" data-tip={$LL.routers.type.remove()}>
-			<button
-				class="btn btn-error btn-outline btn-square"
-				on:click={(e) => {
-					e.preventDefault();
-					messageBoxs.open({
-						title: $LL.components.graphql.table.removeModalTitle(),
-						buttonName: $LL.components.graphql.table.removeBtn(),
-						buttonType: 'error',
-						confirm: () => {
-							removeRows();
-							return true;
-						}
-					});
-				}}
-			>
-				<Icon src={Trash} class="h-6 w-6" solid />
-			</button>
-		</div>
-	{/if}
-	<div class="tooltip tooltip-bottom" data-tip={$LL.routers.type.remove()}>
-		<button
-			class="btn btn-square md:hidden"
-			on:click={(e) => {
-				e.preventDefault();
-				goto('./user/+');
-			}}
-		>
-			<Icon src={Plus} class="h-6 w-6" solid />
-		</button>
-	</div>
-	<button
-		class="hidden md:btn"
-		on:click={(e) => {
-			e.preventDefault();
-			goto('./user/+');
-		}}
-	>
-		{$LL.routers.type.create()}
-	</button>
-</SectionHead>
-<div class="divider" />
-<Table>
-	<thead>
-		<tr>
-			<th class="z-10">
-				<label>
-					<input
-						type="checkbox"
-						class="checkbox"
-						bind:checked={selectAll}
-						on:change={() => {
-							nodes.forEach((node) => {
-								if (node?.id) {
-									selectedRows[node.id] = selectAll;
-								}
-							});
-						}}
-					/>
-				</label>
-			</th>
-			<StringTh
-				name="name"
-				bind:expression={variables.name}
-				bind:sort={orderBy.name}
-				on:filter={query}
-			/>
-			<StringTh
-				name="login"
-				bind:expression={variables.login}
-				bind:sort={orderBy.login}
-				on:filter={query}
-			/>
-			<StringTh
-				name="password"
-				bind:expression={variables.password}
-				bind:sort={orderBy.password}
-				on:filter={query}
-			/>
-			<td />
-		</tr>
-	</thead>
-	{#if $QueryUserConnection.isFetching}
-		<TableLoading />
-	{:else}
-		<tbody>
-			{#each nodes as node}
-				{#if node && node.id}
-					<tr class="hover">
-						<th class="z-10">
-							<label>
-								<input type="checkbox" class="checkbox" bind:checked={selectedRows[node.id]} />
-							</label>
-						</th>
-						<StringTd
-							name="name"
-							bind:value={node.name}
-							on:save={() => updateField({ id: node?.id, name: node?.name })}
-							error={errors[node.id]?.name}
-						/>
-						<StringTd
-							name="login"
-							bind:value={node.login}
-							on:save={() => updateField({ id: node?.id, login: node?.login })}
-							error={errors[node.id]?.login}
-						/>
-						<StringTd
-							name="password"
-							bind:value={node.password}
-							on:save={() => updateField({ id: node?.id, password: node?.password })}
-							error={errors[node.id]?.password}
-						/>
-						<td>
-							<div class="tooltip" data-tip={$LL.components.graphql.table.editBtn()}>
-								<button
-									class="btn btn-square btn-ghost btn-xs"
-									on:click={(e) => {
-										e.preventDefault();
-										if (node) {
-											goto(`./user/${node.id}`);
-										}
-									}}
-								>
-									<Icon src={PencilAlt} solid />
-								</button>
-							</div>
-							<div class="tooltip" data-tip={$LL.components.graphql.table.removeBtn()}>
-								<button
-									class="btn btn-square btn-ghost btn-xs"
-									on:click={(e) => {
-										e.preventDefault();
-										messageBoxs.open({
-											title: $LL.components.graphql.table.removeModalTitle(),
-											buttonName: $LL.components.graphql.table.removeBtn(),
-											buttonType: 'error',
-											confirm: () => {
-												if (node?.id) {
-													removeRow(node.id);
-												}
-												return true;
-											}
-										});
-									}}
-								>
-									<Icon src={Trash} solid />
-								</button>
-							</div>
-						</td>
-					</tr>
-				{/if}
-			{/each}
-		</tbody>
-	{/if}
-</Table>
-<div class="divider" />
-<Pagination
-	bind:pageNumber
-	bind:pageSize
-	{totalCount}
-	on:pageChange={query}
-	on:sizeChange={query}
+<Table
+	nodes={$QueryUserConnection.data?.userConnection?.edges?.map((edge) => edge?.node)}
+	totalCount={$QueryUserConnection.data?.userConnection?.totalCount || 0}
+	isFetching={$QueryUserConnection.isFetching}
+	on:fetch={fetch}
+	on:mutation={mutation}
 />
-{#if $QueryUserConnection.errors}
-	{notifications.error($LL.message.requestFailed())}
-{/if}
