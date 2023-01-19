116f3bf7fab0e318d99d004c4657811f3860677d
Kai <doukai.java@gmail.com>
2023-01-19 10:24:28 +0800

    update

--- packages/codegen/src/index.ts
+++ packages/codegen/src/index.ts
@@ -1,6 +1,6 @@
 import type { PluginFunction, Types } from "@graphql-codegen/plugin-helpers";
 import type { TypeScriptPluginConfig } from './config.js';
-import { GraphQLObjectType, isListType, isNonNullType, isObjectType, type GraphQLField, type GraphQLOutputType, type GraphQLSchema } from 'graphql';
+import { isListType, isNonNullType, isObjectType, type GraphQLField, type GraphQLOutputType, type GraphQLSchema } from 'graphql';
 import { Liquid } from 'liquidjs'
 
 const engine = new Liquid({
@@ -8,35 +8,7 @@ const engine = new Liquid({
     extname: '.liquid'
 })
 
-
-engine.registerFilter('getFieldType', (type: GraphQLOutputType): GraphQLOutputType => getFieldType(type));
-engine.registerFilter('isConnection', (fieldName: string): boolean => fieldName.slice(-connectionSuffix.length) === connectionSuffix);
-engine.registerFilter('getScalarFields', (type: GraphQLOutputType): GraphQLField<any, any, any>[] => {
-    const fieldType = getFieldType(type);
-    if (isObjectType(fieldType)) {
-        return Object.values(fieldType.getFields())
-            .filter(field => !isObjectType(getFieldType(field.type)))
-            .filter(field => !aggregateSuffix.some(suffix => field.name.slice(-suffix.length) === suffix));
-    }
-    throw new Error('scalar field not exsit in object');
-});
-engine.registerFilter('getConnectionScalarFields', (type: GraphQLOutputType): GraphQLField<any, any, any>[] => {
-    const fieldType = getFieldType(type);
-    if (isObjectType(fieldType)) {
-        const edgesType = getFieldType(fieldType.getFields().edges.type);
-        if (isObjectType(edgesType)) {
-            const nodeType = getFieldType(edgesType.getFields().node.type);
-            if (isObjectType(nodeType)) {
-                return Object.values(nodeType.getFields())
-                    .filter(field => !isObjectType(getFieldType(field.type)))
-                    .filter(field => !aggregateSuffix.some(suffix => field.name.slice(-suffix.length) === suffix));
-            }
-        }
-    }
-    throw new Error('scalar field not exsit in node');
-});
-
-const isConnection = (fieldName?: string): boolean => fieldName?.slice(-connectionSuffix.length) === connectionSuffix;
+const isConnection = (fieldName?: string): boolean => { return fieldName?.slice(-connectionSuffix.length) === connectionSuffix };
 
 const getFieldType = (type: GraphQLOutputType): GraphQLOutputType => {
     if (isListType(type) || isNonNullType(type)) {
@@ -45,7 +17,7 @@ const getFieldType = (type: GraphQLOutputType): GraphQLOutputType => {
     return type;
 }
 
-const getScalarFields = (field?: GraphQLField<any, any, any>): GraphQLField<any, any, any>[] => {
+const getScalarFields = (field?: GraphQLField<any, any, any>): GraphQLField<any, any, any>[] | undefined => {
     if (field?.type) {
         const fieldType = getFieldType(field.type);
         if (isConnection(field?.name)) {
@@ -68,7 +40,7 @@ const getScalarFields = (field?: GraphQLField<any, any, any>): GraphQLField<any,
             }
         }
     }
-    throw new Error(`scalar field not exsit in ${field?.name}`);
+    return undefined;
 }
 
 const aggregateSuffix = ["Count", "Sum", "Avg", "Max", "Min", "Aggregate"];
@@ -79,15 +51,15 @@ export const plugin: PluginFunction<TypeScriptPluginConfig, Types.ComplexPluginO
     documents: Types.DocumentFile[],
     config: TypeScriptPluginConfig
 ) => {
+
     const operationFields = schema.getQueryType()?.getFields();
     if (operationFields) {
         const field = Object.keys(operationFields)
             .map(key => operationFields[key])
             .find(field => field.name === config.fieldName);
         if (field) {
-            console.log({ name: field?.name, type: JSON.stringify(getScalarFields(field)) })
             return {
-                content: engine.renderFileSync('query', { name: field?.name, args: field?.args, isConnection: isConnection(field?.name), fields: getScalarFields(field) }),
+                content: engine.renderFileSync('query', { name: field?.name, args: field?.args, isConnection: isConnection(field.name), fields: getScalarFields(field) }),
             };
         }
 
