4c2bfbb180e90ef174b858cf1d7c96531b515697
doukai <doukai.java@gmail.com>
2024-12-11 16:53:56 +0800

    Update FieldTds.svelte

--- packages/ui-graphql/src/lib/components/table/FieldTds.svelte
+++ packages/ui-graphql/src/lib/components/table/FieldTds.svelte
@@ -2,17 +2,17 @@
 	import { type Field } from '@graphace/graphql';
 
 	export let fields: Field[] = [];
-	export let node: unknown = {};
+	export let node: unknown | null | undefined = {};
 	export let nullString: string = '-';
 	export let className: string = '';
 </script>
 
 {#each fields || [] as field}
 	{#if field.fields}
-		<svelte:self node={node[field.name]} fields={field.fields} {className} />
-	{:else if node[field.name] == null}
+		<svelte:self node={node?.[field.name]} fields={field.fields} {className} />
+	{:else if node?.[field.name] == null}
 		<td class={className}>{nullString}</td>
 	{:else}
-		<td class={className}>{node[field.name]}</td>
+		<td class={className}>{node?.[field.name]}</td>
 	{/if}
 {/each}
