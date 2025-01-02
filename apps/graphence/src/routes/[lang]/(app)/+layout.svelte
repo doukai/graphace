14feae94f30268315c6ecfd74c566f1ba6119673
doukai <doukai.java@gmail.com>
2025-01-02 17:18:04 +0800

    update

--- apps/graphence/src/routes/[lang]/(app)/+layout.svelte
+++ apps/graphence/src/routes/[lang]/(app)/+layout.svelte
@@ -140,36 +140,59 @@
 			<UserMenu slot="option3" />
 		</NavBar>
 		<main class="flex-1 max-w-[100vw] px-2 py-2 lg:max-w-[calc(100vw-20rem)]">
-			<Breadcrumbs>
-				<li>
-					<a
-						href={null}
-						on:click|preventDefault={(e) => {
-							init(`/${$locale}`);
-						}}
-					>
-						<span>{$LL.graphence.path.home()}</span>
-					</a>
-				</li>
-				{#each $history as page}
+			<div class="flex justify-between items-center">
+				<Breadcrumbs>
 					<li>
 						<a
 							href={null}
 							on:click|preventDefault={(e) => {
-								to(page.url);
+								init(`/${$locale}`);
 							}}
 						>
-							{#if page.type === PageType.CREATE}
-								{$LL.graphence.path.create({ name: page.name })}
-							{:else if page.type === PageType.SELECT}
-								{$LL.graphence.path.select({ name: page.name })}
-							{:else}
-								{page.name}
-							{/if}
+							<span>{$LL.graphence.path.home()}</span>
 						</a>
 					</li>
-				{/each}
-			</Breadcrumbs>
+					{#each $history as page}
+						<li>
+							<a
+								href={null}
+								on:click|preventDefault={(e) => {
+									to(page.url);
+								}}
+							>
+								{#if page.type === PageType.CREATE}
+									{$LL.graphence.path.create({ name: page.name })}
+								{:else if page.type === PageType.SELECT}
+									{$LL.graphence.path.select({ name: page.name })}
+								{:else}
+									{page.name}
+								{/if}
+							</a>
+						</li>
+					{/each}
+				</Breadcrumbs>
+				<details class="dropdown dropdown-end">
+					<summary class="btn btn-sm">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							class="inline-block w-5 h-5 stroke-current"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+							/>
+						</svg>
+					</summary>
+					<ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+						<li><a>Item 1</a></li>
+						<li><a>Item 2</a></li>
+					</ul>
+				</details>
+			</div>
 			<slot />
 			<MessageBoxs />
 			<Notifications />
--- packages/ui/src/lib/components/Breadcrumbs.svelte
+++ packages/ui/src/lib/components/Breadcrumbs.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 </script>
-<div class="px-6 mb-2 max-w-fit">
+<div class="px-6 max-w-fit">
 	<div class="text-sm breadcrumbs">
 		<ul>
 			<slot />
