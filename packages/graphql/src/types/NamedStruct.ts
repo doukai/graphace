96e75ebc1e923f2358647d2f56a904364c3f8bcd
doukai <doukai.java@gmail.com>
2025-03-13 17:28:11 +0800

    Update NamedStruct.ts

--- packages/graphql/src/types/NamedStruct.ts
+++ packages/graphql/src/types/NamedStruct.ts
@@ -1,15 +1,11 @@
 import { StringExpression } from "./StringExpression.js";
 
 export type NamedStruct = {
-    [id: string]: string;
-} & {
     name?: string | null | undefined;
     description?: string | null | undefined;
 }
 
 export type QueryNamedStructArgs = {
-    [id: string]: StringExpression | number | undefined;
-} & {
     name?: StringExpression | undefined,
     first?: number | undefined
 }
