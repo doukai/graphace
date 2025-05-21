c8a98612963e90768e5fe9d741b9de39b77340c5
doukai <doukai.java@gmail.com>
2025-05-21 16:59:11 +0800

    update

---
+++ packages/ui-graphql/src/lib/components/input/IDLink.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import type { Errors } from '@graphace/commons';
+	import { StringLink } from '.';
+
+	export let id: string | undefined = undefined;
+	export let name: string | undefined = undefined;
+	export let value: string | (string | null | undefined)[] | null | undefined = undefined;
+	export let list: boolean = false;
+	export let errors: Errors | undefined = undefined;
+	export let readonly = false;
+	export let disabled = false;
+	export let placeholder: string = '';
+	export let zIndex: number | undefined = 0;
+	let className: string | undefined = undefined;
+	export { className as class };
+</script>
+
+<StringLink
+	{id}
+	{name}
+	bind:value
+	{list}
+	{errors}
+	{readonly}
+	{disabled}
+	{placeholder}
+	{zIndex}
+	class={className}
+	on:save
+/>
