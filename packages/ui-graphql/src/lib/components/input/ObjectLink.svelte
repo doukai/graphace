00b1f95e035996f9b544a5fd9268930de1ef54af
doukai <doukai.java@gmail.com>
2025-05-28 17:14:03 +0800

    update

--- packages/ui-graphql/src/lib/components/input/ObjectLink.svelte
+++ packages/ui-graphql/src/lib/components/input/ObjectLink.svelte
@@ -2,68 +2,61 @@
 	import { getContext, createEventDispatcher } from 'svelte';
 	import type { Readable } from 'svelte/store';
 	import type { Errors } from '@graphace/commons';
-	import type { NamedStruct } from '@graphace/graphql';
 	import type { TranslationFunctions } from '~/i18n/i18n-types';
 
 	export let id: string | undefined = undefined;
 	export let path: string;
-	export let name: string;
-	export let namedStruct: NamedStruct | (NamedStruct | null | undefined)[] | null | undefined =
-		undefined;
+	export let name: string | undefined = undefined;
+	export let value: any = undefined;
+	export let textFieldName: string | undefined = undefined;
 	export let errors: Errors | undefined = undefined;
 	export let disabled = false;
 	let className: string | undefined = 'btn-link p-0';
 	export { className as class };
 
 	const LL = getContext<Readable<TranslationFunctions>>('LL');
-
-	let _namedStruct = namedStruct;
-	namedStruct = undefined;
+	export let text: string | undefined = $LL.ui_graphql.table.editBtn();
 
 	const dispatch = createEventDispatcher<{
-		goto: { path: string; name: string };
+		goto: { path: string; name: string | undefined };
 	}>();
+
+	$: if (Array.isArray(value)) {
+		if (textFieldName) {
+			if (value.length > 3) {
+				text = value
+					.slice(0, 3)
+					.map((node) => node?.[textFieldName])
+					.filter((name) => name !== null)
+					.join(',')
+					.concat('...');
+			} else {
+				text = value
+					.map((node) => node?.[textFieldName])
+					.filter((name) => name !== null)
+					.join(',');
+			}
+			value = undefined;
+		}
+	} else if (value) {
+		if (textFieldName) {
+			text = value?.[textFieldName] + '';
+		}
+		value = undefined;
+	}
 </script>
 
-{#if _namedStruct && Array.isArray(_namedStruct) && _namedStruct.length > 0}
-	<a
-		{id}
-		class="link {errors ? 'link-error' : ''}"
-		href={null}
-		on:click|preventDefault={(e) => {
-			dispatch('goto', { path, name });
-		}}
-	>
-		{#if _namedStruct && _namedStruct.length > 3}
-			{_namedStruct
-				.map((item) => item.name)
-				.slice(0, 3)
-				.join(',')
-				.concat('...')}
-		{:else}
-			{_namedStruct.map((item) => item.name).join(',')}
-		{/if}
-	</a>
-{:else if _namedStruct && !Array.isArray(_namedStruct)}
-	<a
-		{id}
-		class="link {errors ? 'link-error' : ''}"
-		href={null}
-		on:click|preventDefault={(e) => {
-			dispatch('goto', { path, name });
-		}}
-	>
-		{_namedStruct.name}
-	</a>
+{#if disabled}
+	{text}
 {:else}
-	<button
+	<a
 		{id}
-		{disabled}
-		class="btn {errors ? 'btn-error' : ''} {className}"
+		class="link {errors ? 'link-error' : ''} {className}"
+		href={null}
 		on:click|preventDefault={(e) => {
 			dispatch('goto', { path, name });
 		}}
 	>
-		{$LL.ui_graphql.table.editBtn()}
-	</button>
+		{text}
+	</a>
 {/if}
