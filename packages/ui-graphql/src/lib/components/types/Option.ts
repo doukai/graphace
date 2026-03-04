22fbc9ec3c470a8ef983135382cc39d4e8d1ea7f
doukai <doukai.java@gmail.com>
2026-03-04 22:45:17 +0800

    update

--- packages/ui-graphql/src/lib/components/types/Option.ts
+++ packages/ui-graphql/src/lib/components/types/Option.ts
@@ -1,10 +1,10 @@
-export type Option<T, R> = {
-    readonly?: ((value?: T | undefined) => boolean) | undefined;
-    disabled?: ((value?: T | undefined) => boolean) | undefined;
-    hidden?: ((value?: T | undefined) => boolean | undefined);
-    hiddenCol?: ((args: R, tab: any) => boolean | undefined);
-    required?: ((value?: T | undefined) => boolean) | undefined;
-    validate?: ((value: T) => Promise<string[]>) | undefined;
-    onChange?: ((fieldValue: any, value?: T | null | undefined) => Promise<T | null | undefined>) | undefined;
-    props?: ((value?: T | undefined) => Record<string, Record<string, any>>) | undefined;
-}
+export type Option<L, V, R, F, A, P> = {
+    readonly?: ((value?: V | undefined, fieldArg?: A) => boolean) | undefined;
+    disabled?: ((value?: V | undefined, fieldArg?: A) => boolean) | undefined;
+    hidden?: ((value?: V | undefined, fieldArg?: A) => boolean | undefined);
+    hiddenCol?: ((args: R, tab?: string | undefined, fieldArg?: A) => boolean | undefined);
+    required?: ((value?: V | undefined) => boolean) | undefined;
+    validate?: (($LL: L, value: V) => Promise<string[]>) | undefined;
+    onChange?: (($LL: L, fieldValue: F, value?: V | null | undefined, fieldArg?: A) => Promise<V | null | undefined>) | undefined;
+    props?: (($LL: L, value?: V | undefined, fieldArg?: A) => Record<string, Record<string, P>>) | undefined;
+};
